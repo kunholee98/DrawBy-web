b94f12f62decadeb834dd5275cc22caa7ee1c8eb
kunholee_98 <dlrjsgh750@naver.com>
2021-10-27 18:09:11 +0900

    large avatar icon

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
 const SAvatar = styled.div`
-  width: 25px;
-  height: 25px;
-  border-radius: 15px;
+  width: ${(props) => (props.lg ? "30px" : "25px")};
+  height: ${(props) => (props.lg ? "30px" : "25px")};
+  border-radius: 50%;
   background-color: #2c2c2c;
+  overflow: hidden;
 `;
 
 const Img = styled.img`
-  max-width: 100%;
+  max-width: 150%;
+  max-height: 150%;
+  margin: 0px auto;
 `;
 
-function Avatar({ url = "" }) {
-  return <SAvatar>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
+function Avatar({ url = "", lg = false }) {
+  return <SAvatar lg={lg}>{url !== "" ? <Img src={url} /> : null}</SAvatar>;
 }
 
 export default Avatar;
